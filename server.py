60c622de21b98c196199b7c50535108938d9f353
Eureka Lopes <lopeseureka@gmail.com>
2026-07-06 18:09:44 -0400

    final_project Server.py

---
+++ server.py
@@ -0,0 +1,28 @@
+# Import libraries
+from flask import Flask, redirect, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Instantiate Flask functionality
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+@app.route("/")
+def getemotions():
+    return render_template("index.html")
+
+@app.route("/emotionDetector", methods=["POST", "GET"])
+def detect_emotion():
+    if request.method == 'POST':
+        req = request.get_json()
+        text_to_analyze = req.get('text')
+    
+    if request.method == 'GET':
+        text_to_analyze = request.args.get('text')
+
+    response = emotion_detector(text_to_analyze)
+
+    return response
+
+
